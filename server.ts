41e1a580dfe122857666f828733c1c1f0775913c
Gonzalo <arguellogonzalo97@gmail.com>
2023-05-18 12:20:25 -0300

    Creation automatic roles

--- server.ts
+++ server.ts
@@ -1,7 +1,19 @@
 import express from "express";
+import cors from "cors";
+import dotenv from "dotenv";
+import morgan from "morgan";
+import connectDB from "./api/db";
+import createRoles from "./api/config/initialSetup";
 
 const app = express();
+dotenv.config();
 const PORT = process.env.PORT || 3000;
+connectDB();
+createRoles();
+app.use(express.json());
+app.use(cors());
+app.use(morgan("dev"));
+app.use(express.urlencoded({ extended: true }));
 
 app.get("/", (req, res) => {
   res.send("Hello World");

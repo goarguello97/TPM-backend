7c115edb483c3c60764a5659ff1d25c7528bc827
Gonzalo <arguellogonzalo97@gmail.com>
2023-05-18 12:11:02 -0300

    Models and schemas setup

---
+++ api/models/User.ts
@@ -0,0 +1,40 @@
+import { NextFunction } from "express";
+import mongoose, { model, Schema, Types } from "mongoose";
+import bcrypt from "bcrypt";
+
+const salt = bcrypt.genSaltSync(10);
+
+const UserSchema = new Schema(
+  {
+    username: { type: String, require: true, unique: true },
+    name: { type: String, require: true },
+    lastname: { type: String, require: true },
+    country: String,
+    dateOfBirth: Date,
+    email: { type: String, require: true, unique: true },
+    password: { type: String, require: true },
+    mentor: { type: [Schema.Types.ObjectId], ref: "User" },
+    role: { type: Schema.Types.ObjectId, required: true, ref: "Role" },
+    md: { type: [Schema.Types.ObjectId], ref: "MD" },
+    matchReq: { type: [Schema.Types.ObjectId], ref: "Match" },
+    matchSend: { type: [Schema.Types.ObjectId], ref: "Match" },
+    match: { type: [Schema.Types.ObjectId], ref: "User" },
+    verify: { type: Boolean, default: false },
+    token: String,
+    skills: [String],
+    avatar: String,
+  },
+  {
+    methods: {
+      hash(password: string, salt: string) {
+        return bcrypt.hashSync(password, salt);
+      },
+      validatePassword(entryPass: string) {
+        return bcrypt.compareSync(entryPass, this.password);
+      },
+    },
+    versionKey: false,
+  }
+);
+
+export default model("User", UserSchema);
---
+++ api/schemas/UserSchema.ts
@@ -0,0 +1,55 @@
+import { Schema } from "express-validator";
+import emailUnique from "../helpers/emailUnique"
+import usernameUnique from "../helpers/userUnique";
+
+export const user: Schema = {
+  username: {
+    notEmpty: { errorMessage: "Username is required" },
+    isLength: {
+      errorMessage: "Minimum 3 characters and maximum 30 characters",
+      options: { min: 3, max: 30 },
+    },
+    custom: { options: usernameUnique },
+  },
+  name: {
+    optional: { options: { checkFalsy: true } },
+    isLength: {
+      errorMessage: "Minimum 3 characters and maximum 30 characters",
+      options: { min: 3, max: 30 },
+    },
+  },
+  lastname: {
+    optional: { options: { checkFalsy: true } },
+    isLength: {
+      errorMessage: "Minimum 3 characters and maximum 30 characters",
+      options: { min: 3, max: 30 },
+    },
+  },
+  country: {
+    optional: { options: { checkFalsy: true } },
+    isLength: {
+      errorMessage: "Minimum 3 characters and maximum 30 characters",
+      options: { min: 3, max: 30 },
+    },
+  },
+  dateOfBirth: {
+    optional: { options: { checkFalsy: true } },
+    isDate: { errorMessage: "Please enter a valid date" },
+  },
+  email: {
+    notEmpty: { errorMessage: "Email is required" },
+    isEmail: { errorMessage: "Enter a valid email" },
+    custom: { options: emailUnique },
+  },
+  password: {
+    matches: {
+      errorMessage:
+        "Minimum 8 characters, one uppercase, one number and one special character",
+      options: /^(?=.*?[A-Z])(?=.*?[a-z])(?=.*?[0-9])(?=.*?[#?!@$%^&*-]).{8,}$/,
+    },
+  },
+  skills: {
+    optional: { options: { checkFalsy: true } },
+    isArray: { errorMessage: "Enter the data correctly" },
+  },
+};

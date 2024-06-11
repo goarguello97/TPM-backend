90d59a40bdcd8f28dd0ae8bcf6f791c1b81d7b47
Gonzalo Argüello <arguellogonzalo97@gmail.com>
2024-06-11 20:05:32 -0300

    create middlewares of roles, and their interfaces. Also changes in token config

--- api/interfaces/IUser.ts
+++ api/interfaces/IUser.ts
@@ -1,21 +1,22 @@
 import { Types } from "mongoose";
 
 export interface IUser {
-    username: string;
-    name: string;
-    lastname: string;
-    country: string;
-    dateOfBirth: string;
-    email: string;
-    password: string;
-    mentor: Types.ObjectId[];
-    role: Types.ObjectId;
-    md: Types.ObjectId[];
-    matchReq: Types.ObjectId[];
-    matchSend: Types.ObjectId[];
-    match: Types.ObjectId[];
-    verify: boolean;
-    skills: string[];
-    avatar: Types.ObjectId;
-    tokenRecover: string;
-  }
+  id: string;
+  username: string;
+  name: string;
+  lastname: string;
+  country: string;
+  dateOfBirth: string;
+  email: string;
+  password: string;
+  mentor: Types.ObjectId[];
+  role: Types.ObjectId;
+  md: Types.ObjectId[];
+  matchReq: Types.ObjectId[];
+  matchSend: Types.ObjectId[];
+  match: Types.ObjectId[];
+  verify: boolean;
+  skills: string[];
+  avatar: Types.ObjectId;
+  tokenRecover: string;
+}

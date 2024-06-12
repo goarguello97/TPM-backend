d8f253a9892523a5ecaecddc8d46c7c477a1df58
Gonzalo Argüello <arguellogonzalo97@gmail.com>
2024-06-12 15:27:42 -0300

    add tests for find user

--- test/user.test.ts
+++ test/user.test.ts
@@ -15,6 +15,40 @@ afterAll(async () => {
   await User.deleteMany({});
 });
 
+describe("GET /users/:id", () => {
+  let id, username;
+  let unknowId = "abc1234";
+  beforeEach(async () => {
+    const user = await User.create({
+      username: "fulanito",
+      email: "haxine1712@lapeds.com",
+      password: "Pass-1234",
+    });
+
+    id = user._id;
+    username = user.username;
+  });
+
+  afterEach(async () => {
+    await User.deleteMany({});
+  });
+
+  it("should find a user by id", async () => {
+    const response = await request(app).get(`/api/users/${id}`);
+
+    expect(response.status).toBe(200);
+    expect(response.body).toHaveProperty("_id", id);
+    expect(response.body).toHaveProperty("username", username);
+  });
+
+  it("should not find a user by incorrect id", async () => {
+    const response = await request(app).get(`/api/users/${unknowId}`);
+
+    expect(response.status).toBe(404);
+    expect(response.body.message).toBe("Usuario no encontrado,");
+  });
+});
+
 describe("GET /users", () => {
   afterEach(async () => {
     await User.deleteMany({});
